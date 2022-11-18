2397113993ff3fc961c643d7ef478c0701e45638
palinko91 <arpad.palinkas@gmail.com>
2022-11-18 22:27:56 +0100

    hex decode instead of .as_bytes

--- src/bin/merkletree_soroban_types.rs
+++ src/bin/merkletree_soroban_types.rs
@@ -1,9 +1,10 @@
 // Written in different file so soroban_sdk::Vec not colliding with std::vec::Vec
+use hex;
 use soroban_sdk::{Bytes, Env, Vec};
 
 pub fn str_to_bytes(env: &Env, string: &str) -> Bytes {
-    let src: &[u8] = string.as_bytes();
-    let bytes: Bytes = Bytes::from_slice(&env, src);
+    let src = hex::decode(string).unwrap();
+    let bytes: Bytes = Bytes::from_slice(&env, &src);
     return bytes;
 }
 
@@ -11,8 +12,8 @@ pub fn str_arr_to_bytes(env: &Env, string: &[&str]) -> Vec<Bytes> {
     let arr_length = string.len();
     let mut bytes_vec: Vec<Bytes> = Vec::new(&env);
     for i in 0..arr_length {
-        let src: &[u8] = string[i].as_bytes();
-        let bytes: Bytes = Bytes::from_slice(&env, src);
+        let src = hex::decode(string[i]).unwrap();
+        let bytes: Bytes = Bytes::from_slice(&env, &src);
         bytes_vec.push_back(bytes);
     }
     return bytes_vec;
