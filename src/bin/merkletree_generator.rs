d7e1dbeda95859faf447827765cde23afd253a35
palinko91 <arpad.palinkas@gmail.com>
2022-11-19 22:15:28 +0100

    tests passing

--- src/bin/merkletree_generator.rs
+++ src/bin/merkletree_generator.rs
@@ -1,91 +1,75 @@
-use merkletreers::merkletree::tree::MerkleTree;
-use soroban_sdk::Env;
-mod merkletree_soroban_types;
+use tiny_keccak::{Hasher, Keccak};
+
+fn hash(a: [u8; 32], b: [u8; 32]) -> [u8; 32] {
+    let mut k256 = Keccak::v256();
+    let mut res = [0u8; 32];
+
+    k256.update(&a);
+    k256.update(&b);
+    k256.finalize(&mut res);
+
+    return res;
+}
+
+pub fn verify(leaf: &str, proof: Vec<&str>, root: &str) -> bool {
+    let leaf_b: [u8; 32] = hex::decode(leaf).unwrap().try_into().unwrap();
+    let root_b: [u8; 32] = hex::decode(root).unwrap().try_into().unwrap();
+
+    let mut proof_b: Vec<[u8; 32]> = Vec::new();
+    for i in 0..proof.len() {
+        let htob: [u8; 32] = hex::decode(proof[i]).unwrap().try_into().unwrap();
+        proof_b.push(htob);
+    }
+    println!("Leaf in bytes: {:?}", &leaf_b);
+    println!("Root in bytes: {:?}", &root_b);
+    println!("Proof in bytes: {:?}", &proof_b);
+
+    let mut computedhash = leaf_b;
+    for proofelement in proof_b.into_iter() {
+        if computedhash <= proofelement {
+            // Hash(current computed hash + current element of the proof)
+            computedhash = hash(computedhash, proofelement);
+        } else {
+            // Hash(current element of the proof + current computed hash)
+            computedhash = hash(proofelement, computedhash);
+        }
+        println!("Computed hash bytes: {:?}", &computedhash);
+    }
+
+    computedhash == root_b
+}
 
 fn main() {
-    // The wallets should be whitelisted. The list should contain power of 2 element, so it can be 2,4,8,16,32,64,128,256,512,1024...etc long
-    let wallet_addresses_to_whitelist = vec![
+    // The wallets should be whitelisted. Since the cargos I found not generated me valid merkletree we are using merkletreejs to generate
+    // https://replit.com/@palinko/MerkleTree#index.js
+    // This should be handled by the website usually
+    // https://medium.com/@ItsCuzzo/using-merkle-trees-for-nft-whitelists-523b58ada3f9
+    let _wallet_addresses_to_whitelist = vec![
         "GBADIKRAYTCM5TL34XIFBY6JN5JDDU23OC6TCG2HJIYVNJMCY5WQARPI",
         "GB2NRKJVDMXKJQA7JMQ27FB2HX6CSLCKRFQQVPNXK6QEDOMN2H2N6PSH",
         "GDIKL43MU7AM2MVJNDD63MAKSCOFGCEGRP4CXKXHTV6WR2FFRJHCNWH2",
         "GCUL2IG2KKV6M65Z5ZBW6GD5SQZGBMNI5TKQP5UM6XPRHQM7VETL3WVX",
-        "GBEL4SDQL4NFDSKLZESRESQCKTCWRBGDROAMFRR4BTQVBC72JN7TVZOM",
-        "GBLFJ7MQCE4CF7WYNQBY37FYS2LDUM3GDYZLQN26HAFAH5QZ5CO2TEQI",
-        "GAMBM4OBANJ4DTY3LO4MRNFTVRVJLHROCZ7ZXXMDUUH2PDUMRYSL3VXN",
-        "GDTA4ADTTPHM42ORRW6J533V5WNABUPNZCARMM7PUJ7GLBLK7FO57CBY",
-        "GA3FJNSI5YUFBGADYYZGYPOH5RW7LI74GPXVJ7QCBRRKSPCPOOMHEPVZ",
-        "GDWQKDEUUUSJRKHOHKHZDYIXCMCKYZT7Q74M7GM3EJJLMSHXD4IFZWZA",
-        "GAYD3HMA5X6E3PDODZEXJL7HVGTARVYKTR7LNY63E5XNY4YNSHYKE5GO",
-        "GCLLWN6TAPKLPCBMHGWPFE2BFKUGMYDTUHLDPHDTCLKGZQWRPWNM7K23",
-        "GCBKRHRFLCJXLDQUCCNLQWCQ22TUWN6OYU4NU362FFX3VFWXNK7IBZVM",
-        "GAC2TCTSYOHU6W6GT2KRZE2VSWIIVRU7DLTQI7TR22P5CE4Z5D7IPD5Z",
-        "GAEV2UDWTA7A6RAQFFOFIFKGUHN2JUNHVMMJVMSK4FYDTXZLCO43QNZX",
-        "GDL4OY7PC4FSH22XV3TEDUBVP4B3DUO3K2CWY4I45P2M5LDYJV3V5H6G",
-        "GBGPJKREV6MDO53QMKOOUSSRZOGRKI5F26FYFL7JYDGFQ3SM37EQKWBQ",
-        "GCD4OH3HXXM3CBXRDA7ALULI5QETLSF4NGZUKUYKG4CK3CGTJ24GF7XZ",
-        "GCBXVKCGMMIM5PYYC65Z4K42AFFPDHTCNBKMSO23SEM65KIU2BNEKJVX",
-        "GDPLYZX4JL6LQT6YMJK6S56YRQ74EIYS7AEVUT5Y3VVZGQJBXZQKK7BT",
-        "GBEI2C2Y5UCKA5SLO62FQIQ6BX3G3W5H7FD3PGIW5F72B2VZCZUHD5NO",
-        "GD532HCAZMSJVJHNCVYY2ML65IS42NQ5Q4E7OBKS3SB25B5DID3YYGR3",
-        "GCBIVJBAJDLJ2UO7ZT2NYVZWT2TV6P2TUB6RWXZUIKSX7EPTQA4EWHYU",
-        "GDG2ITIOPGEKJNZSFNX3TAOS6YQZZ253SGS5CXLBOTSVJVJWSEVIWXW5",
-        "GCN4QHQDR3UXYBUVMO33NUFUYA5WVFGID6CS3HLC2SQRDNYLGWRMPV43",
-        "GBIPSW3DLEFWIPCXCZW3I3D5YZRB2PHO3GWC22TIWINIBUWFWN6F53FI",
-        "GALI7NOQ2J7XH2B2TPWALQV66ARW2TDP52YUMZIEJE3D3DQD2QZ3Y7HI",
-        "GDAAMAPO5KNGP4BT2M3FDO4HNZZ2DQLXVNAGM6VHDV2TWIWHF6OVFNCP",
-        "GA52JNXWFJ3NUMFA5R75SYL4QWDYZRX2RYHLZKUGYZA7FTXWGT7GNMXZ",
-        "GCGGCUDXW3YLVT5U267DZQHA4KTM6ZK5RYRHSRILZTN2BHT7AVHSJL63",
-        "GDZCTYOFXC5O4CWOF6WD6XAZFQFUCNGBIO3AYQFIMCPICPXALR2RY2MU",
-        "GC7NIXKDPNXY4WLJ7AMN5E3TQELZSKGITIYXA5LLX3I3VGID7SKVSE43",
     ];
 
-    let tree = MerkleTree::new(wallet_addresses_to_whitelist.clone());
-    let root_hash = &tree.root;
-    let leafs = &tree.leafs;
-    assert_eq!(
-        &wallet_addresses_to_whitelist.len(),
-        &leafs.len(),
-        "The wallet address and the leaf nodeas amount not matching!"
+    // Result
+    // └─ 6285ee21931bf045649c3d733fe2dbde77f28dcd5ccf8c35c3b6194897322ce1
+    //    ├─ 29616ec520fa85b9b8fb2036dde47a4d7391e20ff46407b27200912630eee127
+    //    │  ├─ 6e23528f84bce52570b74ec5f1b73f3d16f985f9257885fb27a35d9c147452e2
+    //    │  └─ e314b422cadcde46a2a819189c6d40f63bc78e77d70690f25a806dbd91a9c612
+    //    └─ 55103311da71177355114e49d6c1698decad87788d6daad983ba9fa157f6a3db
+    //       ├─ 42b53059a452803a7a22f368fb74b45ec77f6453573587b4bcfbdfeb65534357
+    //       └─ 1e6fbd9f127151e0b4ea1c1c105f71f96dd6500dca258b0c990ed60e7fef4c53
+
+    let root = "6285ee21931bf045649c3d733fe2dbde77f28dcd5ccf8c35c3b6194897322ce1";
+    let leaf = "6e23528f84bce52570b74ec5f1b73f3d16f985f9257885fb27a35d9c147452e2";
+    let proof = vec![
+        "e314b422cadcde46a2a819189c6d40f63bc78e77d70690f25a806dbd91a9c612",
+        "55103311da71177355114e49d6c1698decad87788d6daad983ba9fa157f6a3db",
+    ];
+
+    let verify_result = verify(leaf, proof, root);
+    println!(
+        "Verification result (expected: true) = {:?}",
+        &verify_result
     );
-
-    println!("The whole tree: \n{:?}\n\n", &tree);
-    println!("The root is: \n{:?}\n\n", &root_hash);
-
-    // https://medium.com/@ItsCuzzo/using-merkle-trees-for-nft-whitelists-523b58ada3f9
-    // Under Website Implementation part, but this time did in Rust
-    let claiming_address = &wallet_addresses_to_whitelist[0];
-    let proof = &tree.proof(claiming_address);
-    println!("The proof is: \n{:?}\n\n\n", &proof);
-
-    // Let's convert into Soroban Bytes and Vec<Bytes> types
-    let env = Env::default();
-    let tree_copy = MerkleTree::new(wallet_addresses_to_whitelist.clone());
-    let root_bytes = merkletree_soroban_types::str_to_bytes(&env, &tree_copy.root);
-    println!("Root in bytes: \n{:?}\n\n\n", root_bytes);
-    let leaf_bytes = merkletree_soroban_types::str_to_bytes(&env, &tree_copy.leafs[0]);
-    println!("Leaf in bytes: \n{:?}\n\n\n", leaf_bytes);
-    let mut proof_array: Vec<&str> = Vec::new();
-    let proof_len = proof.len();
-    for i in 0..proof_len {
-        let r = match &proof[i].r {
-            None => "None",
-            Some(string) => string,
-        };
-
-        let l = match &proof[i].l {
-            None => "None",
-            Some(string) => string,
-        };
-
-        if r != "None" {
-            proof_array.push(r);
-        }
-
-        if l != "None" {
-            proof_array.push(l);
-        }
-    }
-
-    let proof_bytes = merkletree_soroban_types::str_arr_to_bytes(&env, &proof_array);
-    println!("Proof in bytes vec: \n{:?}", proof_bytes);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
 #![no_std]
-use soroban_sdk::{assert_with_error, contracterror, contractimpl, vec, Bytes, Env, Vec};
+use soroban_sdk::{contractimpl, Bytes, Env, Vec};
 use tiny_keccak::{Hasher, Keccak};
-
-#[contracterror]
-#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-#[repr(u32)]
-pub enum Error {
-    InvalidMultiProof = 1,
-}
 
 pub struct MerkleProof;
 
@@ -29,99 +22,11 @@ impl MerkleProof {
 
     pub fn processprf(env: Env, proof: Vec<Bytes>, leaf: Bytes) -> Bytes {
         let mut computedhash: Bytes = leaf;
-        let mut i = 0;
-        while i < proof.len() {
+        for i in 0..proof.len() {
             computedhash =
                 Self::hashpair(env.clone(), computedhash, proof.get_unchecked(i).unwrap());
-            i += 1;
         }
         return computedhash;
-    }
-
-    /// Returns true if the `leaves` can be simultaneously proven to be a part of a merkle tree defined by
-    /// `root`, according to `proof` and `proofFlags` as described in {processMultiProof}.
-    ///
-    /// CAUTION: Not all merkle trees admit multiproofs. See {processMultiProof} for details.
-
-    pub fn multiprfvr(
-        env: Env,
-        proof: Vec<Bytes>,
-        proofflags: Vec<bool>,
-        root: Bytes,
-        leaves: Vec<Bytes>,
-    ) -> bool {
-        return Self::prcsmp(env, proof, proofflags, leaves) == root;
-    }
-
-    /// Returns the root of a tree reconstructed from `leaves` and sibling nodes in `proof`. The reconstruction
-    /// proceeds by incrementally reconstructing all inner nodes by combining a leaf/inner node with either another
-    /// leaf/inner node or a proof sibling node, depending on whether each `proofFlags` item is true or false
-    /// respectively.
-    ///
-    /// CAUTION: Not all merkle trees admit multiproofs. To use multiproofs, it is sufficient to ensure that:
-    /// 1) the tree is complete (but not necessarily perfect),
-    /// 2) the leaves to be proven are in the opposite order they are in the tree (i.e., as seen from right to
-    /// left starting at the deepest layer and continuing at the next layer).
-
-    pub fn prcsmp(env: Env, proof: Vec<Bytes>, proofflags: Vec<bool>, leaves: Vec<Bytes>) -> Bytes {
-        // This function rebuild the root hash by traversing the tree up from the leaves. The root is rebuilt by
-        // consuming and producing values on a queue. The queue starts with the `leaves` array, then goes onto the
-        // `hashes` array. At the end of the process, the last hash in the `hashes` array should contain the root of
-        // the merkle tree.
-        let leaves_len: u32 = leaves.len();
-        let total_hash: u32 = proofflags.len();
-
-        // Check proof validity.
-        assert_with_error!(
-            &env,
-            leaves_len + proof.len() - 1 == total_hash,
-            Error::InvalidMultiProof
-        );
-
-        // The xxxPos values are "pointers" to the next value to consume in each array. All accesses are done using
-        // `xxx[xxxPos++]`, which return the current value and increment the pointer, thus mimicking a queue's "pop".
-        let mut hashes: Vec<Bytes> = vec![&env, Bytes::from_slice(&env, &[total_hash as u8])];
-        let mut leaf_pos: u32 = 0;
-        let mut hash_pos: u32 = 0;
-        let mut proof_pos: u32 = 0;
-        // At each step, we compute the next hash using two values:
-        // - a value from the "main queue". If not all leaves have been consumed, we get the next leaf, otherwise we
-        //   get the next hash.
-        // - depending on the flag, either another value for the "main queue" (merging branches) or an element from the
-        //   `proof` array.
-        let mut i = 0;
-        while i < total_hash {
-            let a: Bytes = if leaf_pos < leaves_len {
-                leaf_pos += 1;
-                leaves.get_unchecked(leaf_pos).unwrap()
-            } else {
-                hash_pos += 1;
-                hashes.get_unchecked(hash_pos).unwrap()
-            };
-
-            let b: Bytes = if proofflags.get_unchecked(i).unwrap() == true && leaf_pos < leaves_len
-            {
-                leaf_pos += 1;
-                leaves.get_unchecked(leaf_pos).unwrap()
-            } else if proofflags.get_unchecked(i).unwrap() == true && leaf_pos >= leaves_len {
-                hash_pos += 1;
-                hashes.get_unchecked(hash_pos).unwrap()
-            } else {
-                proof_pos += 1;
-                proof.get_unchecked(proof_pos).unwrap()
-            };
-
-            hashes.insert(i, Self::hashpair(env.clone(), a, b));
-            i += 1;
-        }
-
-        if total_hash > 0 {
-            return hashes.get_unchecked(total_hash - 1).unwrap();
-        } else if leaves_len > 0 {
-            return leaves.get_unchecked(0).unwrap();
-        } else {
-            return proof.get_unchecked(0).unwrap();
-        }
     }
 
     fn hashpair(env: Env, a: Bytes, b: Bytes) -> Bytes {
